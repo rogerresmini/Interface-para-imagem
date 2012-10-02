f8e5c6793fa8c5f2c69684bfab98c31f73d9f2ef
Roger <roger@LIC-Ubuntu.(none)>
2012-10-02 10:25:45 -0400

    primeiro teste

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    view = new QGraphicsView(ui->graphicsView);
+    scene = new QGraphicsScene();
+
+    connect()
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+MainWindow::loadImage(){
+    imgPath = QFileDialog(this, "Abrir Imagem", QDir::currentPath(), tr(".png .jpg .jpeg"));
+
+    //view->set
+
+    view->setScene(scene);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QString>
+#include <QFileDialog>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +18,12 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QGraphicsView *view;
+    QGraphicsScene *scene;
+    QString imgPath;
+
+public slots:
+    void loadImage();
     
 private:
     Ui::MainWindow *ui;
